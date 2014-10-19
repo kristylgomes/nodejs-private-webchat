b04bed4593d15d3bb0ae8b5e5328880822089189
Kristyl Macbook Air local <kristylgomes@hotmail.com>
2014-10-19 11:24:38 -0700

    Updating notifications

--- public/js/chat.js
+++ public/js/chat.js
@@ -174,43 +174,45 @@ $(function(){
 			scrollToBottom();
 			//var notification = new Notification(data.msg);
 			//notifyMe();
-				function notifyMe() {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
+		function notifyMe() {
+	  // Let's check if the browser supports notifications
+	  if (!("Notification" in window)) {
+	    alert("This browser does not support desktop notification");
+	  }
 
-  // Let's check if the user is okay to get some notification
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification(data.msg, {
-  	body: 'from Kristyl Orlovich chatsite'});
-  }
+	  // Let's check if the user is okay to get some notification
+	  else if (Notification.permission === "granted") {
+	    // If it's okay let's create a notification
+	    var notification = new Notification(data.msg, {
+	  	body: 'from Kristyl Orlovich chatsite'});
+	  	setTimeout(function(){notification.close()}, 2500);
+	  }
 
-  // Otherwise, we need to ask the user for permission
-  // Note, Chrome does not implement the permission static property
-  // So we have to check for NOT 'denied' instead of 'default'
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // Whatever the user answers, we make sure we store the information
-      if (!('permission' in Notification)) {
-        Notification.permission = permission;
-      }
+	  // Otherwise, we need to ask the user for permission
+	  // Note, Chrome does not implement the permission static property
+	  // So we have to check for NOT 'denied' instead of 'default'
+	  else if (Notification.permission !== 'denied') {
+	    Notification.requestPermission(function (permission) {
+	      // Whatever the user answers, we make sure we store the information
+	      if (!('permission' in Notification)) {
+	        Notification.permission = permission;
+	      }
 
-      // If the user is okay, let's create a notification
-      if (permission === "granted") {
-            var notification = new Notification(data.msg, {
-  	body: 'from Kristyl Orlovich chatsite'});
-      }
-    });
-  }
+	      // If the user is okay, let's create a notification
+	      if (permission === "granted") {
+	        var notification = new Notification(data.msg, {
+	  	body: 'from Kristyl Orlovich chatsite'});
+	  	setTimeout(function(){notification.close()}, 2500);
+	      }
+	    });
+	  }
 
-  // At last, if the user already denied any notification, and you 
-  // want to be respectful there is no need to bother them any more.
-}
-notifyMe();
+  		// At last, if the user already denied any notification, and you 
+ 		 // want to be respectful there is no need to bother them any more.
 		}
-	});
+		notifyMe();
+			}
+	 });
 
 	textarea.keypress(function(e){
 

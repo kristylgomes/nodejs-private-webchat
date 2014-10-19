b60c699a714c82fbc54671e29aeff88ef9250040
Kristyl Macbook Air local <kristylgomes@hotmail.com>
2014-10-18 21:25:04 -0700

    Adding more notification functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -165,6 +165,40 @@ $(function(){
 		}
 	});
 
+	function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check if the user is okay to get some notification
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("New Message!", {
+  	body: 'from Kristyl Orlovich chatsite'});
+  }
+
+  // Otherwise, we need to ask the user for permission
+  // Note, Chrome does not implement the permission static property
+  // So we have to check for NOT 'denied' instead of 'default'
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // Whatever the user answers, we make sure we store the information
+      if (!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+
+      // If the user is okay, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Chats will be sent");
+      }
+    });
+  }
+
+  // At last, if the user already denied any notification, and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
 	socket.on('receive', function(data){
 
 		showMessage('chatStarted');
@@ -172,7 +206,8 @@ $(function(){
 		if(data.msg.trim().length) {
 			createChatMessage(data.msg, data.user, data.img, moment());
 			scrollToBottom();
-			var notification = new Notification('New message!');
+			// var notification = new Notification('New message!');
+			notifyMe();
 		}
 	});
 
